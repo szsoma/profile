9cc5b03c9b44719fcc877fe9be9dc753d1c3fd1b
szsoma <soma.szoboszlai@innonic.com>
2017-02-21 21:43:53 +0100

    Add jQuery text-align to the .pros-block. It's awesome! :)

---
+++ js/addclass.js
@@ -0,0 +1,16 @@
+(function($) {
+    var $window = $(window),
+        $html = $('.pros-block');
+
+    function resize() {
+        if ($window.width() > 768) {
+            return $html.addClass('text-left');
+        }
+
+        $html.removeClass('text-left');
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+})(jQuery);
